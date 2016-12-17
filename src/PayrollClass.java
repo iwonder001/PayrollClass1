e21c76d9268515fa4fc96428fc312da3601af4f9
Kristen <kcarethers@gmail.com>
2016-12-17 18:43:22 -0500

    Initial commit

---
+++ src/PayrollClass.java
@@ -0,0 +1,15 @@
+
+public class PayrollClass {
+	
+	private int employeeid;
+	private int hours;
+	private double payRate;
+	private double wages;
+
+	public PayrollClass (int emp, int hr, double pay, double wag) {
+		employeeid = emp;
+		hours = hr;
+		payRate = pay;
+		wages = wag;
+	}
+}
